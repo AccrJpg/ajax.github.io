cceeeb288e238875694f9eb2f14353054619378a
Kacper <112724545+AccrJpg@users.noreply.github.com>
2022-11-19 13:10:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+console.log('pstryk pstryk');
+
+
+let btnGetData = document.getElementById('get-data');
+
+const getData = () => {
+
+    fetch(' https://akademia108.pl/api/ajax/get-post.php')
+        .then(res => res.json())
+        .then(data => {
+
+
+            let pId = document.createElement('p');
+            let pUserId = document.createElement('p');
+            let pTitle = document.createElement('p');
+            let pBody = document.createElement('p');
+
+            pId.innerText = `Post ID: ${data.id}`;
+            pUserId.innerText = `UserId: ${data.userId}`;
+            pTitle.innerText = `Title: ${data.title}`;
+            pBody.innerText = `Body: ${data.body}`;
+
+                document.body.appendChild(pId);
+                document.body.appendChild(pUserId);
+                document.body.appendChild(pTitle);
+                document.body.appendChild(pBody);
+
+        })
+
+                .catch(error => {
+                    console.log(error);
+                })
+
+
+
+    console.log('getData()');
+}
+
+
+btnGetData.addEventListener('click', getData);
